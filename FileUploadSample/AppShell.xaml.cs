22555a6cf21374106c005f87ff07706a32dab4be
Ron Weatherston <rweatherston@utilantllc.onmicrosoft.com>
2024-08-29 12:35:12 -0400

    Add project

---
+++ FileUploadSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FileUploadSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FileUploadSample/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace FileUploadSample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            WebView.Source = new HtmlWebViewSource
+            {
+                Html =
+                    """
+                        <html>
+                            <body>
+                                <div>
+                                    <h3>Testing file input type</h3>
+                                    <form>
+                                        <label for="file">Select a file: </label>
+                                        <input name="file" type="file" />
+                                    </form>
+                                </div>
+                            </body>
+                        </html>
+                    """
+            };
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
